160758222ee239a6dd3259431b148c13f78f5b08
dpromstk2000-lab <dpromstk2000@gmail.com>
2026-08-11 19:51:23 +0900

    Add files via upload

--- config.js
+++ config.js
@@ -6,12 +6,13 @@ window.DPRO_CONFIG = Object.freeze({
   defaultFacilityCode: "dpro_sales",
   sessionStorageKey: "dpro_sales_session_v3",
   indexVersion: "SALES-50-INDEX-20260811",
-  releaseVersion: "DPRO-SALES-V1.1-3-20260811",
+  releaseVersion: "DPRO-SALES-V1.1-4-20260811",
   productMasterVersion: "DPRO-SALES-V1.1-3-MASTER50-20260811",
-  ownerVersion: "SALES-48-OWNER-20260811",
+  ownerVersion: "DPRO-SALES-V1.1-4-OWNER-20260811",
   ownerIpadVersion: "SALES-12-OWNER-IPAD-20260804",
   staffVersion: "DPRO-SALES-V1.1-2-STAFF-20260811",
   teamVersion: "SALES-48-TEAM-20260811",
+  channelResearchVersion: "DPRO-SALES-V1.1-4-CHANNEL-RESEARCH-20260811",
   systemCheckVersion: "SALES-49-SYSTEM-CHECK-20260811",
   timezone: "Asia/Tokyo"
 });
